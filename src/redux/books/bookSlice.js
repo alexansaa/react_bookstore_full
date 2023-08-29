54a5791ff05cdf255299cf7e5b239dd031433d05
alexansaa <alexan.saa@gmail.com>
2023-08-29 15:28:26 -0500

    Requirements implementations

---
+++ src/redux/books/bookSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  bookItems: [],
+};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      const newBook = action.payload
+      state.bookItems.addItem(newBook);
+    },
+    removeBook: (state, action) => {
+      const bookName = action.payload
+      state.bookItems = state.bookItems.filter((item) => item.bname !== bookName )
+    }
+  }
+});
+
+export const {addItem, removeBook} = bookSlice.actions;
+export default bookSlice.reducer;
